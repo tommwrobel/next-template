93e5e1fdf801035745f01749ebfb7cc5a07950db
Tomasz Wróbel <tomasz.wrobel@symmetrical.ai>
2023-06-09 14:54:23 +0200

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import "./globals.css";
+import { Inter } from "next/font/google";
+import { ReactNode } from "react";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Create Next App Template",
+  description:
+    "Starting point for developing a Next.js application with pre-configured basic settings",
+};
+
+type RootLayoutProps = {
+  children: ReactNode;
+};
+
+const RootLayout = ({ children }: RootLayoutProps) => {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+};
+
+export default RootLayout;
---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import styles from "./page.module.css";
+
+const Home = () => {
+  return (
+    <main className={styles.main}>
+      <h1>Home</h1>
+      <p>Sample HomePage</p>
+    </main>
+  );
+};
+
+export default Home;
